0ba652ea39c6a921baf4d0f31129a2c12c878dd4
zaid <kkzyxw@gmail.com>
2026-07-01 07:28:24 +0530

    view students added

--- pages/view_students.php
+++ pages/view_students.php
@@ -1,2 +1,32 @@
 <?php
-echo "Hello";
+require_once "../config/database.php";
+
+$result = $conn->query("SELECT * FROM students");
+
+?>
+<table border="2" cellpadding="12px">
+
+<tr>
+    <th>ID</th>
+    <th>Name</th>
+    <th>Email</th>
+    <th>City</th>
+    <th>Course</th>
+    <th>Action</th>
+</tr>
+<?php
+    while ($row = $result->fetch_assoc()) : ?>
+
+    <tr>
+        <td><?= $row['id']?></td>
+        <td><?= $row['name']?></td>
+        <td><?= $row['email']?></td>
+        <td><?= $row['city']?></td>
+        <td><?= $row['course']?></td>
+        <td>
+            <a href="#">Edit</a> | <a href="#">Delete</a>
+        </td> 
+    </tr>
+    <?php endwhile;?>
+
+</table>
